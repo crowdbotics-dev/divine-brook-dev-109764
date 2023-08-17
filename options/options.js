e21024d4430317a0be2b9e0cceabfa47f0dbc752
Crowdbotics <engineering@crowdbotics.com>
2023-08-17 12:14:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-brook-dev-109764",
+  url: "https://divine-brook-dev-109764.botics.co",
+  api: "https://divine-brook-dev-109764.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
